1a4ef4248d4bce699dd43c1bc3ccb9c88b82898c
Nadav Misgav <nadav.misgav@gmail.com>
2020-03-30 12:23:31 +0300

    change to gz

--- extract_all.sh
+++ extract_all.sh
@@ -1,8 +1,8 @@
 cd src_files
-tar_files=`find . -name '*.tar'`
+tar_files=`find . -name '*.tar.gz'`
 
 for tar_file in $tar_files;
 do
-    tar xf $tar_file
+    tar xzf $tar_file
     rm -f $tar_file
 done;
